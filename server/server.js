d77fd77775191c288c21a03c866ab0f382f9dfe0
varvoid <jairocampana10001@gmail.com>
2018-12-12 22:17:53 -0500

    RestServer - Usuario

--- server/server.js
+++ server/server.js
@@ -1,38 +1,23 @@
+require('./config/config');
+
 const express = require('express');
+const mongoose = require('mongoose');
+
 const app = express();
 const bodyParser = require('body-parser');
-require('./config/config');
+
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }));
 
 // parse application/json
 app.use(bodyParser.json());
 
-app.get('/usuarios', function (req, res) {
-    res.json('get Usuario');
-});
-app.post('/usuarios', function (req, res) {
-    let persona = req.body;
-    if(persona.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    } else {
-        res.json({
-            persona
-        });
-    }
+app.use(require('./routes/usuario'));
 
-});
-app.put('/usuarios/:id', function (req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    });
-});
-app.delete('/usuarios', function (req, res) {
-    res.json('delete Usuario');
+mongoose.connect(process.env.URLDB, (err, resp) => {
+    if (err) throw err;
+
+    console.log('Base de datos ONLINE');
 });
 
 app.listen(process.env.PORT, () => {
